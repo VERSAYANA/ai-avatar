4a7085d0d986d7c4a112cd8e97b818456539270f
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-02-04 21:04:20 +0330

    Make font sizes more responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,16 @@ export default function Home() {
         <title>Home</title>
       </Head>
       <main className="h-full min-h-screen w-full overflow-x-hidden dark:bg-black">
-        <div className="flex h-full min-h-screen w-full flex-col items-center justify-center overflow-x-hidden p-6">
-          <h1 className="text-3xl font-bold text-gray-900/95 dark:text-gray-50/95 md:text-6xl">
-            Turn me into anything you want
-          </h1>
-          <p className="mb-6 mt-10 text-xl font-medium text-gray-800/80 dark:text-gray-100/80">
-            Use your creativty to create a prompt or use my prompt generator
-            tool for inspiration
-          </p>
+        <div className="container mx-auto flex h-full min-h-screen w-full flex-col items-center justify-center overflow-x-hidden p-6">
+          <div className="flex flex-col px-2">
+            <h1 className="text-center text-4xl font-bold text-gray-900/95 dark:text-gray-50/95 md:text-5xl lg:text-6xl">
+              Turn me into anything you want
+            </h1>
+            <p className="mb-7 mt-10 text-center text-base font-medium text-gray-800/80 dark:text-gray-100/80 sm:text-lg md:text-xl">
+              Use your creativty to create a prompt or use my prompt generator
+              tool for inspiration
+            </p>
+          </div>
           <div className="flex w-full max-w-3xl flex-col gap-4">
             <div className="flex flex-col">
               {/* <label
@@ -41,13 +43,13 @@ export default function Home() {
                 target={'_blank'}
                 rel="noreferrer"
                 type="button"
-                className="inline-flex justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 drop-shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-100 hover:dark:bg-gray-900"
+                className="inline-flex justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 drop-shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 dark:border-neutral-700 dark:bg-neutral-900 dark:text-gray-100 hover:dark:bg-neutral-800"
               >
                 Prompt Generator
               </a>
               <button
                 type="submit"
-                className="inline-flex justify-center rounded-md border border-transparent bg-purple-600 py-2 px-4 text-sm font-medium text-white drop-shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 dark:bg-purple-700 hover:dark:bg-purple-800 "
+                className="inline-flex justify-center rounded-md border border-transparent bg-purple-600 py-2 px-4 text-sm font-medium text-white drop-shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 dark:bg-purple-700 hover:dark:bg-purple-600 "
               >
                 Generate
               </button>
