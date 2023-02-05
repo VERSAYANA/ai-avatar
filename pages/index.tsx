a57bd105305b3231ccfb91e6ff01aaf83f5e979d
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-02-05 12:54:05 +0330

    Impl UI for generating images using prompt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,49 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import { X } from 'react-feather'
+import Image from 'next/image'
 
 export default function Home() {
+  const [generatedImage, setGeneratedImage] = useState('')
+  const [prompt, setPrompt] = useState('')
+
+  console.log(generatedImage)
+
+  const retry = (estimatedTime: number) => {
+    window.setTimeout(() => {
+      generate()
+    }, estimatedTime * 1000)
+  }
+
+  const generate = async () => {
+    console.log('versayana, ' + prompt)
+    try {
+      const res = await fetch('/api/generate', {
+        method: 'POST',
+        body: JSON.stringify({ prompt: 'versayana, ' + prompt }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      if (res.status === 503) {
+        const data = await res.json()
+        if (data?.estimated_time) {
+          retry(data.estimated_time + 1)
+        }
+      } else if (res.ok) {
+        const data = await res.json()
+        if (data?.image) {
+          setGeneratedImage(data.image)
+        }
+      } else {
+        const err = await res.json()
+        console.error(err)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <>
       <Head>
@@ -19,12 +62,6 @@ export default function Home() {
           </div>
           <div className="flex w-full max-w-3xl flex-col gap-4">
             <div className="flex flex-col">
-              {/* <label
-                htmlFor="prompt"
-                className="mb-2 ml-1 block text-base font-medium text-gray-800/80"
-              >
-                Prompt
-              </label> */}
               <div className="flex h-12 w-full rounded-md drop-shadow-sm">
                 <span className="inline-flex cursor-default items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-4 text-sm text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300">
                   versayana,{' '}
@@ -33,8 +70,16 @@ export default function Home() {
                   type={'text'}
                   name="prompt"
                   id="prompt"
-                  className="block w-full flex-1 rounded-none rounded-r-md border border-gray-300 focus:border-purple-500 focus:ring-purple-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 sm:text-sm"
+                  value={prompt}
+                  onChange={(e) => setPrompt(e.target.value)}
+                  className="rounded-r-0 block w-full flex-1 rounded-none border border-r-0 border-gray-300 focus:border-purple-500 focus:ring-purple-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 sm:text-sm"
                 />
+                <button
+                  onClick={() => setPrompt('')}
+                  className="inline-flex items-center justify-center rounded-md rounded-l-none border border-gray-300 bg-white py-2 px-3 text-sm font-medium text-gray-700 drop-shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-slate-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-gray-100 hover:dark:bg-neutral-800 focus:dark:ring-offset-slate-900"
+                >
+                  <X size={24} />
+                </button>
               </div>
             </div>
             <div className="flex justify-end gap-2">
@@ -43,29 +88,32 @@ export default function Home() {
                 target={'_blank'}
                 rel="noreferrer"
                 type="button"
-                className="inline-flex justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 drop-shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 dark:border-neutral-700 dark:bg-neutral-900 dark:text-gray-100 hover:dark:bg-neutral-800"
+                className="inline-flex justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 drop-shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-slate-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-gray-100 hover:dark:bg-neutral-800 focus:dark:ring-offset-slate-900"
               >
                 Prompt Generator
               </a>
               <button
-                type="submit"
-                className="inline-flex justify-center rounded-md border border-transparent bg-purple-600 py-2 px-4 text-sm font-medium text-white drop-shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 dark:bg-purple-700 hover:dark:bg-purple-600 "
+                onClick={generate}
+                type="button"
+                className="inline-flex justify-center rounded-md border border-transparent bg-purple-600 py-2 px-4 text-sm font-medium text-white drop-shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-slate-50 dark:bg-purple-700 hover:dark:bg-purple-600 focus:dark:ring-offset-slate-900 "
               >
                 Generate
               </button>
             </div>
-            {/* <div className="mt-1 flex rounded-md shadow-sm">
-              <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-                http://
-              </span>
-              <input
-                type="text"
-                name="company-website"
-                id="company-website"
-                className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                placeholder="www.example.com"
-              />
-            </div> */}
+            {generatedImage && (
+              <div className="flex justify-center py-6">
+                <a href={generatedImage} download={`${prompt}`}>
+                  <Image
+                    className="rounded-2xl drop-shadow-sm"
+                    unoptimized={true}
+                    src={generatedImage}
+                    alt={prompt}
+                    width={512}
+                    height={512}
+                  />
+                </a>
+              </div>
+            )}
           </div>
         </div>
       </main>
